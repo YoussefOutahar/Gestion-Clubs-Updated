19108a3057e015cb85322d846d5ec413d6e8aa55
Youssef Outahar <youssefoutahar1@gmail.com>
2023-12-10 21:07:56 +0100

    Add create links to nav bar

--- src/app/LandingPage/Components/nav-bar/nav-bar.component.ts
+++ src/app/LandingPage/Components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class NavBarComponent implements OnInit {
   constructor() {}
