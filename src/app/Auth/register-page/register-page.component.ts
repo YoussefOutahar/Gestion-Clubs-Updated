6ca85468d41132f2541071ed5c6f0b995e668991
Youssef Outahar <youssefoutahar1@gmail.com>
2023-12-28 18:22:17 +0100

    Refactor register-page component HTML and TypeScript files

--- src/app/Auth/register-page/register-page.component.ts
+++ src/app/Auth/register-page/register-page.component.ts
@@ -7,13 +7,18 @@ import { Router } from '@angular/router';
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent implements OnInit {
-
   constructor(private router: Router) {}
 
   form = new FormGroup({
-    uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
     email: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    field: new FormControl('', [Validators.required]),
+    year: new FormControl('', [Validators.required]),
   });
 
   get f() {
@@ -22,10 +27,7 @@ export class RegisterPageComponent implements OnInit {
 
   submit() {
     console.log(this.form.value);
-    this.router.navigate(['/dashboard']);
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
